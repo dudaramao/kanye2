1c49df87728c9c0e508238385ed66060625caa85
dudaramao <141442251+dudaramao@users.noreply.github.com>
2024-11-08 09:05:38 -0300

    .

---
+++ perguntas.js
@@ -0,0 +1,101 @@
+criaCartao(
+    'História',
+    'Quem matou Getúlio Vargas?',
+    'Getúlio se suicidou com um tiro no peito'
+)
+criaCartao(
+    'História',
+    'Quando começou a segunda guerra mundial?',
+    '1939'
+)
+criaCartao(
+    'História',
+    'Quem foi o primeiro presiente do Brasil?',
+    'Marechal Teodoro'
+)
+criaCartao(
+    'Hitória',
+    'Qual país colonizou o Brasil?',
+    'Portugal'
+)
+criaCartao(
+    'História',
+    'Em que ano começou a guerra fria?',
+    '1947'
+)
+criaCartao(
+    'História',
+    'Onde se originou a doença da peste negra?',
+    'Ásia Central'
+)
+criaCartao(
+    'Hisória',
+    'Em que ano foi a proclamação da república no Brasil?',
+    '15.11.1889'
+)
+criaCartao(
+    'História',
+    'Em que ano a família real veio para o Brasil?',
+    '1807-1808'
+)
+criaCartao(
+    'História',
+    'Qual foi a primeira capital do Brasil?',
+    'Salvador'
+)
+criaCartao(
+    'História',
+    'Quem foi o líder nazista?',
+    'Hitler'
+)
+criaCartao(
+    'Geografia',
+    'Quantos paises tem o continete africano?',
+    '55 píses'
+)
+criaCartao(
+    'Geografia',
+    'Qual a capital da Ítalia?',
+    'Roma'
+)
+criaCartao(
+    'Geografia',
+    'Quantos paises tem na América do Sul?',
+    '12 Paises'
+)
+criaCartao(
+    'Geografia',
+    'Qual o menor país do mundo?',
+    'Vaticano'
+)
+criaCartao(
+    'Geografia',
+    'Qual o país mais populoso do mundo?',
+    'Índia'
+)
+criaCartao(
+    'Geografia',
+    'Qual a mior cidade do mundo?',
+    'Tóquio'
+)
+criaCartao(
+    'Geografia',
+    'Qual a capital mais fria do Brasil?',
+    'Curitiba'
+)
+criaCartao(
+    'Geografia',
+    'Em que continente está o Brasil?',
+    'América'
+)
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'Paris'
+)
+criaCartao(
+    'Geografia',
+    'Qual o país mais extenso do mundo?',
+    'Rússia'
+)
+
